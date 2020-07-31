453e62a1690b72747f861c6806198a8f5ce5b065
jesusnboone <jesusnboone@gmail.com>
2020-07-31 17:06:00 -0500

    updating heroku, got rid of Develop folder

--- server.js
+++ server.js
@@ -11,18 +11,18 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-const notes = require('./Develop/db/db.json')
+const notes = require('./db/db.json')
 
 
 // Routes
 // =============================================================
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/Develop/public/index.html'));
+  res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
 app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, '/Develop/public/notes.html'));
+  res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
 app.get('/api/notes', (req, res) => {
@@ -30,7 +30,7 @@ app.get('/api/notes', (req, res) => {
 });
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/Develop/public/index.html'));
+  res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
 app.post('/api/notes', (req, res) => {
