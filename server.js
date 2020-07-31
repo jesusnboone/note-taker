f471e973757b54f863d99c488dcd2514aceeb6a2
jesusnboone <jesusnboone@gmail.com>
2020-07-31 14:20:11 -0500

    changed paths

--- server.js
+++ server.js
@@ -17,11 +17,11 @@ const notes = require('./Develop/db/db.json')
 // =============================================================
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/public/index.html'));
+  res.sendFile(path.join(__dirname, '/develop/public/index.html'));
 });
 
 app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, '/public/notes.html'));
+  res.sendFile(path.join(__dirname, '/develop/public/notes.html'));
 });
 
 app.get('/api/notes', (req, res) => {
