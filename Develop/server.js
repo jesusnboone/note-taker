1b7c24eec56fca4edfbadfa79b30fae27ca2125f
jesusnboone <jesusnboone@gmail.com>
2020-07-31 13:01:57 -0500

    fixed page route

--- Develop/server.js
+++ Develop/server.js
@@ -16,12 +16,12 @@ const notes = require('../Develop/db/db.json')
 // Routes
 // =============================================================
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
 app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, 'notes.html'));
+  res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
 app.get('/api/notes', (req, res) => {
